90183aeb194cf992b620f552fb5e023f8ee8bfa3
Henry D Chipman <hchipman@uw.edu>
2026-09-02 15:44:01 -0700

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.kt
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var p1 = head
+        var p2 = head?.next
+
+        while (p1 != null && p2 != null) {
+            if (p1 == p2) {
+                return true
+            }
+
+            p1 = p1.next
+            p2 = p2.next?.next
+        }
+        
+        return false
+    }
+}
